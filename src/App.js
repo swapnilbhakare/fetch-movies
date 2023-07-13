6c51fff79f57e85dfb1695c699bfc787407d570c
swapnilbhakare <swapnilbhakare7@gmail.com>
2023-07-13 21:41:49 +0530

    making apps error proof done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -6,6 +6,9 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isRetrying, setIsRetrying] = useState(false);
+
   const dummyMovies = [
     {
       id: 1,
@@ -23,32 +26,78 @@ function App() {
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.py4e.com/api/films/");
-    const data = await response.json();
+    setError(null);
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Something went wrong...Retrying");
+      }
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setError(false);
+      setTimeout(() => {
+        fetchMoviesHandler();
+      }, 5000);
+    }
+  }
+  useEffect(() => {
+    if (isRetrying) {
+      fetchMoviesHandler();
+      setIsRetrying(false);
+    }
+  }, [isRetrying]);
+
+  function cancelHandler() {
+    setIsRetrying(false);
+    setError(null);
     setIsLoading(false);
+  }
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = (
+      <p>
+        {error}
+        <br />
+        <br />
+        <button onClick={cancelHandler}>Cancel</button>
+      </p>
+    );
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Movies not found</p>}
-        {isLoading && <p>Loading....</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
 
 export default App;
+
+// What do 5XX responses mean? Explain a few of them.
+
+// How does try catch work?
+
+// What happens when we throw an error?
